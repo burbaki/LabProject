db8f866aa05bcae12dbcc95deb6752edc9e238e3
Egor Lenko <sipperp@gmail.com>
2016-04-06 12:44:07 +0300

    Update classes Money.java and Weight.java Fix#1'

--- boxClasses/Money.java
+++ boxClasses/Money.java
@@ -2,62 +2,29 @@ package boxClasses;
 
 public class Money {
 
-    // существует интерфейс компарабле. реализуй клас используя его .
+    
     private double money;
 
-    /**
-     *
-     * @param Double
-     */
+   
+    
     public Money(double value) {
-         this.money = value;
-        
+        this.money = value;    
     }
 
-    public Double getMoney() {
-        // TODO - implement Money.getMoney
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     *
-     * @param Double
-     */
-    /**
-     *
-     * @param Money
-     */
     public Money addMoney(Money money) {
-      this.money += money.money; 
-      return this; 
+        this.money += money.money; 
+         return this; 
     }
 
-    /**
-     *
-     * @param Money
-     */
     public Money subMoney(Money money) {
         this.money -= money.money; 
-      return this; 
+         return this; 
     }
 
-    public String toString() {
-        // TODO - implement Money.toString
-        throw new UnsupportedOperationException();
-    }
-
-    // функция называется примерно так. общие требования при первом меньшем -1 при первом большем +1  при равности 0
-    public int Compare(Money money)
-        {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
     
-public Double toDouble()
-{
-return this.money;
+     public Double toDouble() {
+         return this.money;
 }
-    public void setMoney(Money money) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+
 
 }
--- boxClasses/Weight.java
+++ boxClasses/Weight.java
@@ -2,51 +2,30 @@ package boxClasses;
 
 public class Weight {
 
+    
 	private double weight;
 
-	/**
-	 * 
-	 * @param Double
-	 */
+        
+        
 	public Weight(double value) {
-           this.weight = value;
-           
-	}
+            this.weight = value;
+        }
 
 	public double getWeight() {
-		return this.weight;
+             return this.weight;
 	}
 
-	/**
-	 * 
-	 * @param Double
-	 */
-	public void setWeight(int Double) {
-		// TODO - implement Weight.setWeight
-		throw new UnsupportedOperationException();
-	}
 
-	/**
-	 * 
-	 * @param Weight
-	 */
 	public Weight addWeight(Weight weight) {
-                  this.weight += weight.weight; 
-                  return this;
+            this.weight += weight.weight; 
+             return this;
 	}
 
-	/**
-	 * 
-	 * @param Weight
-	 */
+
 	public Weight subWeight(Weight weight) {
-		this.weight -= weight.weight; 
-                return this;
+	    this.weight -= weight.weight; 
+             return this;
 	}
 
-	public String toString() {
-		// TODO - implement Weight.toString
-		throw new UnsupportedOperationException();
-	}
 
 }
